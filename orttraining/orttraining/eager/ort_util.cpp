b71dddb58c176f5b1bf08addbcb8c3474fdc34e1
root <root@QTM-ANHNGUYEN-1.northamerica.corp.microsoft.com>
2022-02-08 15:45:30 -0800

    fix-ort-eager-harcoded-memory-info

--- orttraining/orttraining/eager/ort_util.cpp
+++ orttraining/orttraining/eager/ort_util.cpp
@@ -25,14 +25,13 @@ void CreateMLValue(onnxruntime::AllocatorPtr alloc,
                   onnxruntime::DataTypeImpl::GetType<onnxruntime::Tensor>()->GetDeleteFunc());
 }
 
-void CreateMLValue(void* data_ptr, onnxruntime::MLDataType element_type, const std::vector<int64_t>& dims, OrtValue* p_mlvalue) {
+void CreateMLValue(void* data_ptr, onnxruntime::MLDataType element_type, const std::vector<int64_t>& dims,
+  const OrtMemoryInfo& memory_info, OrtValue* p_mlvalue) {
   onnxruntime::TensorShape shape(dims);
-  OrtMemoryInfo *cpu_info;
-  Ort::ThrowOnError(Ort::GetApi().CreateCpuMemoryInfo(OrtArenaAllocator, OrtMemTypeDefault, &cpu_info));
   std::unique_ptr<onnxruntime::Tensor> p_tensor = std::make_unique<onnxruntime::Tensor>(element_type,
                                                                       shape,
                                                                       data_ptr,
-                                                                      *cpu_info);
+                                                                      memory_info);
   
   p_mlvalue->Init(p_tensor.release(),
                   onnxruntime::DataTypeImpl::GetType<onnxruntime::Tensor>(),
